daae9c8987c5416757259c9194fdf34d0f38e42f
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-08-21 18:32:33 +0600

    updated page, navbar and global css

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,20 @@ import Navbar from "./Navbar/page";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar></Navbar>
+    <div className="min-h-screen w-full bg-[#020617] relative">
+      {/* Emerald Radial Glow Background */}
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          backgroundImage: `radial-gradient(circle 500px at 50% 300px, rgba(16,185,129,0.35), transparent)`,
+        }}
+      />
+
+      {/* Components */}
+      <div className="relative z-10">
+        <Navbar />
+      </div>
     </div>
   );
 }
+

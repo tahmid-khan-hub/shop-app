14be6dda5ec83e50edde7537836295d85708b407
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-08-22 21:05:45 +0600

    updated footer and its design

--- src/app/Footer/page.js
+++ src/app/Footer/page.js
@@ -1,56 +1,69 @@
+import Image from "next/image";
+import brand from "../../../public/shopping-bag.png";
+
 export default function Footer() {
   return (
-    <footer className="sm:footer-horizontal text-neutral-content items-center p-4 max-w-[1300px] mx-auto">
-      <div className="footer">
-        <aside className="grid-flow-col items-center">
-          <svg
-            width="36"
-            height="36"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            fillRule="evenodd"
-            clipRule="evenodd"
-            className="fill-current"
+    <footer className="p-6 max-w-[1300px] mx-auto mt-16">
+      <div className="flex flex-col sm:flex-row justify-between items-center gap-4">
+        {/* Logo & Copyright */}
+        <div className="flex items-center gap-3">
+          <Image className="w-8" src={brand} alt="shopApp Logo" />
+          <a className="text-xl font-semibold text-emerald-500">
+            Shop<span className="text-2xl">A</span>pp
+          </a>
+        </div>
+        <p className="text-sm text-gray-500">
+          © {new Date().getFullYear()} ShopApp. All rights reserved.
+        </p>
+
+        {/* Social Icons */}
+        <div className="flex gap-6 text-gray-600">
+          <a
+            href="https://www.google.com/search?gs_ssp=eJzj4tLP1TfIyDMsS05XYDRgdGDwYi8pzywpSS0CAFnOB00&q=twitter&oq=twi&gs_lcrp=EgZjaHJvbWUqEwgBEC4YgwEYxwEYsQMY0QMYgAQyBggAEEUYOTITCAEQLhiDARjHARixAxjRAxiABDIKCAIQABixAxiABDIQCAMQLhjHARixAxjRAxiABDIKCAQQABixAxiABDINCAUQABiDARixAxiABDIMCAYQABgUGIcCGIAEMgcIBxAAGI8CMgcICBAAGI8C0gEJMjU4OGowajE1qAIIsAIB8QVUeV6nWa9uPQ&sourceid=chrome&ie=UTF-8"
+            aria-label="Twitter"
+            className="hover:text-emerald-500 transition-colors"
           >
-            <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-          </svg>
-          <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
-        </aside>
-        <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-          <a>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
+              fill="currentColor"
               viewBox="0 0 24 24"
-              className="fill-current"
             >
-              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+              <path d="M24 4.557a9.93 9.93 0 0 1-2.828.775c1.017-.61 1.798-1.574 2.165-2.724a9.936 9.936 0 0 1-3.127 1.195c-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045C7.691 7.58 4.063 5.62 1.634 2.641c-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 0 1-2.224.084c.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
             </svg>
           </a>
-          <a>
+          <a
+            href="https://www.google.com/search?gs_ssp=eJzj4tTP1TewzEouKzZg9GKvzC8tKU1KBQA_-AaN&q=youtube&oq=y&gs_lcrp=EgZjaHJvbWUqDAgCEC4YJxiABBiKBTIGCAAQRRg8MgYIARBFGDwyDAgCEC4YJxiABBiKBTIGCAMQRRhAMg8IBBBFGDkYgwEYsQMYgAQyBggFEEUYPDIGCAYQRRg8MgYIBxBFGD3SAQgxNjI2ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8"
+            aria-label="YouTube"
+            className="hover:text-emerald-500 transition-colors"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
+              fill="currentColor"
               viewBox="0 0 24 24"
-              className="fill-current"
             >
-              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0C.488 3.45.029 5.804 0 12c.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.267 4.356-2.621 4.385-8.816-.029-6.196-.488-8.55-4.385-8.816zM9 16V8l8 4-8 4z" />
             </svg>
           </a>
-          <a>
+          <a
+            href="https://www.google.com/search?gs_ssp=eJzj4tLP1TfIyK1MKy5TYDRgdGDw4khLTE5Nys_PBgBmYAfL&q=facebook&oq=fa&gs_lcrp=EgZjaHJvbWUqEggBEC4YJxjHARjRAxiABBiKBTIGCAAQRRg8MhIIARAuGCcYxwEY0QMYgAQYigUyBggCEEUYOTISCAMQABhDGIMBGLEDGIAEGIoFMgYIBBBFGDwyBggFEEUYPTIGCAYQRRg8MgYIBxBFGDzSAQgzMDUwajBqN6gCALACAA&sourceid=chrome&ie=UTF-8"
+            aria-label="Facebook"
+            className="hover:text-emerald-500 transition-colors"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
+              fill="currentColor"
               viewBox="0 0 24 24"
-              className="fill-current"
             >
-              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+              <path d="M9 8H6v4h3v12h5V12h3.642L18 8h-4V6.333C14 5.378 14.192 5 15.115 5H18V0h-3.808C10.596 0 9 1.583 9 4.615V8z" />
             </svg>
           </a>
-        </nav>
+        </div>
       </div>
     </footer>
   );

4a3073297eca066dc83a394288d1818a28099617
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-08-22 19:18:06 +0600

    updated login page and added log in and log out condition

--- src/app/Navbar/page.js
+++ src/app/Navbar/page.js
@@ -1,30 +1,55 @@
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 export default function Navbar() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
   return (
     <div className="navbar sticky top-0 z-50 bg-background/55 backdrop-blur-md text-white">
       <div className="max-w-[1300px] w-[96%] mx-auto flex justify-between items-center">
+        {/* Navbar Start */}
         <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>Products</a>
+              </li>
+              <li>
+                <a>Dashboard</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
+          <a className="btn btn-ghost text-xl">ShopApp</a>
+        </div>
+
+        {/* Navbar Center */}
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
             <li>
               <a>Home</a>
             </li>
@@ -36,24 +61,24 @@ export default function Navbar() {
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">ShopApp</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>Products</a>
-          </li>
-          <li>
-            <a>Dashboard</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Login</a>
-      </div>
+
+        {/* Navbar End */}
+        <div className="navbar-end">
+          {!session ? (
+            <button className="btn" onClick={() => router.push("/Login")}>
+              Login
+            </button>
+          ) : (
+            <button
+              className="btn "
+              onClick={() =>
+                signOut({ callbackUrl: "http://localhost:3000" })
+              }
+            >
+              Log out
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
